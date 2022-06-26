e5ecbcbbed79f7069f9fadef42598f86a87b74b4
Maksim <gorev18091993@gmail.com>
2022-06-26 13:27:04 -0500

    cs50 adieu assignment

---
+++ week_4_Libraries/adieu/adieu.py
@@ -0,0 +1,22 @@
+adieu = "Adieu, adieu, to "
+names = []
+
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+    except (EOFError, KeyboardInterrupt):
+        print()
+        break
+
+if len(names) == 1:
+        print(adieu + names[0])
+elif len(names) == 2:
+        print(adieu + names[0] + " and " + names[1])
+else:      
+    for n in range(len(names) -1):
+        if n == 0:
+            adieu += names[n]
+        else:
+            adieu += ", " + names[n]
+    print(adieu + ", and " + names[-1])
