2cf58aa974f3af1a4800c6ab049aa8872134e355
Maximgitman <gorev18091993@gmail.com>
2022-07-23 19:12:43 -0500

    cs50 python working assignment done

--- week_7_Regular Expressions/working/working.py
+++ week_7_Regular Expressions/working/working.py
@@ -6,10 +6,12 @@ def main():
 
 
 def convert(s):
-    if match := re.search(
+
+    match = re.search(
         r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$",
         s,
-    ):
+    )
+    if match:
         first_time = s.split(" ")[:2]
         second_time = s.split(" ")[3:]
 
@@ -18,15 +20,15 @@ def convert(s):
             split_first = first_time[0].split(":")
             m = split_first[1]
             if first_time[1] == "PM":
-                if first_time[0] == "12":
+                if split_first[0] == "12":
                     h = 12
                 else:
                     h = int(split_first[0]) + 12
             else:
-                if first_time[0] == "12":
+                if split_first[0] == "12":
                     h = 0
                 else:
-                    h = int(split_first[0]) + 12
+                    h = int(split_first[0])
             first_time[0] = f"{h:02}:{m}"
         else:
             m = "00"
@@ -39,7 +41,7 @@ def convert(s):
                 if first_time[0] == "12":
                     h = 0
                 else:
-                    h = int(first_time[0]) + 12
+                    h = int(first_time[0])
             first_time[0] = f"{h:02}:{m}"
 
         # Second time
@@ -47,44 +49,37 @@ def convert(s):
             split_second = second_time[0].split(":")
             m = split_second[1]
             if second_time[1] == "PM":
-                h = int(split_second[0]) + 12
+                if split_second[0] == "12":
+                    h = 12
+                else:
+                    h = int(split_second[0]) + 12
             else:
+                if split_second[0] == "12":
+                    h = 0
+                else:
+                    h = int(split_second[0])
                 h = int(split_second[0])
             second_time[0] = f"{h:02}:{m}"
         else:
             m = "00"
             if second_time[1] == "PM":
-                h = int(second_time[0]) + 12
+                if second_time[0] == "12":
+                    h = 12
+                else:
+                    h = int(second_time[0]) + 12
             else:
-                h = int(second_time[0])
-
+                if second_time[0] == "12":
+                    h = 0
+                else:
+                    h = int(second_time[0])
             second_time[0] = f"{h:02}:{m}"
 
         corrected_time = f"{first_time[0]} to {second_time[0]}"
 
         return corrected_time
+    else:
+        raise ValueError
 
-
-"""
-1. Check if 12 for second time
-
-:( working.py converts "12 AM to 12 PM" to "00:00 to 12:00"
-    expected "00:00 to 12:00...", not "12:00 to 24:00..."
-:( working.py converts "12:00 AM to 12:00 PM" to "00:00 to 12:00"
-    expected "00:00 to 12:00...", not "12:00 to 24:00..."
-:( working.py raises ValueError when given "8:60 AM to 4:60 PM"
-    expected "ValueError", not "None\n"
-:( working.py raises ValueError when given "9AM to 5PM"
-    expected "ValueError", not "None\n"
-:( working.py raises ValueError when given "09:00 to 17:00"
-    expected "ValueError", not "None\n"
-:( working.py raises ValueError when given "9 AM - 5 PM"
-    expected "ValueError", not "None\n"
-:( working.py raises ValueError when given "10:7 AM - 5:1 PM"
-    expected "ValueError", not "None\n"
-:( correct working.py passes all test_working checks
-    expected exit code 0, not 5
-"""
 
 if __name__ == "__main__":
     main()
