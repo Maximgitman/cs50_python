687f31e2768e9ae288f413626022e8268295f8fc
Maximgitman <gorev18091993@gmail.com>
2022-08-15 16:07:26 -1000

    cs50 python seassons assignment done

--- week_8_Object-Oriented Programming/seasons/seasons.py
+++ week_8_Object-Oriented Programming/seasons/seasons.py
@@ -1,47 +1,37 @@
-from datetime import date, datetime
+from datetime import date
 import inflect
 import sys
+import re
 
 
-class Minutes:
-    p = inflect.engine()
 
-    @classmethod
-    def get_minutes(cls, birth_data, today):
-        day_difference = today - birth_data
-        minutes = day_difference.days * 24 * 60
-        words = cls.p.number_to_words(minutes, andword="")
-        return f"{words.capitalize()} minutes"
 
 def main():
-    birth_data = validate(input("Date of Birth: "))
+    birth_data = input("Date of Birth: ")
+
+    try:
+        year, month, day = validating(birth_data)
+    except:
+        sys.exit("Invalid Date")
+
+    formated = date(int(year), int(month), int(day))
     today = date.today()
-    print(Minutes().get_minutes(birth_data, today))
+    print(get_minutes(formated, today))
 
 
-def validate(date_text):
-        try:
-            datetime.strptime(date_text, "%Y-%m-%d")
-            return date.fromisoformat(date_text)
-        except ValueError:
-            sys.exit("Invalid Date")
+def get_minutes(birth_data, today):
+    p = inflect.engine()
+    day_difference = today - birth_data
+    minutes = day_difference.days * 24 * 60
+    words = p.number_to_words(minutes, andword="")
+    return f"{words.capitalize()} minutes"
+
+
+def validating(date_text):
+    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", date_text):
+        year, month, day = date_text.split("-")
+        return year, month, day
 
 
 if __name__ == "__main__":
     main()
-
-    
-    """
-    Five hundred twenty-five thousand, six hundred minutes.
-    One million, fifty-one thousand, two hundred minutes
-    :( Input of "1999-01-01" yields "Five hundred twenty-five thousand, six hundred minutes" when today is 2000-01-01
-    expected "Five hundred t...", not "Twelve million..."
-    :( Input of "2001-01-01" yields "One million, fifty-one thousand, two hundred minutes" when today is 2003-01-01
-        expected "One million, f...", not "Eleven million..."
-    :( Input of "1995-01-01" yields "Two million, six hundred twenty-nine thousand, four hundred forty minutes" when today is 2000-01-1
-        expected "Two million, s...", not "Fourteen milli..."
-    :( Input of "2020-06-01" yields "Six million, ninety-two thousand, six hundred forty minutes" when today is 2032-01-01
-        expected "Six million, n...", not "One million, o..."
-    :( Input of "1998-06-20" yields "Eight hundred six thousand, four hundred minutes" when today is 2000-01-01
-        expected "Eight hundred ...", not "Twelve million..."
-    """
--- week_8_Object-Oriented Programming/seasons/test_seasons.py
+++ week_8_Object-Oriented Programming/seasons/test_seasons.py
@@ -1,1 +1,8 @@
-from seasons import Minutes
+from seasons import validating, get_minutes
+from datetime import date
+
+
+def test_format():
+    assert validating("1999-01-01") == ("1999", "01", "01")
+    assert validating("February 6th, 1993") == None
+    assert validating("1999-3-1") == None
